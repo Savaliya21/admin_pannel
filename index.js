20645838c3c1fa7f4cda9a8985777760b7a7c1d5
Jaydeep_Savaliya <100779071+Savaliya21@users.noreply.github.com>
2022-11-25 09:43:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const Port = 3000;
 const app = express();
 
+app.get("/", (req, res)=>{
+    res.send("hello")
+})
+
+
 app.listen(Port, function(err){
     if(err){
         console.log('somthing wrong');
